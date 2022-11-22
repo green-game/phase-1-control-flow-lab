aff4524d8e883d3d7512c50acb99549c066568b0
Allan Monzon <greengamer.am@gmail.com>
2022-11-22 17:24:06 -0500

    commit tree c6092f88c734fc8c7d965b972b3d17c2e158128c

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(randomNum){
   // Write your code here!
+  
+  if  (randomNum <= 400){
+    return 'This one is on me!';
+}
+  else if (randomNum > 400 && randomNum < 2000){
+    return 'That will be twenty bucks.';
+  }
+  else if (randomNum > 2000 && randomNum < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (randomNum >= 2500){
+    return 'No can do.';
+  }
+
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC"){
+   return "Ok, sounds good."
+  }
+  else {
+    return 'No go.' 
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+if (tip === 'generous'){
+  return'Thank you so much.'
+} else if (tip === 'not as generous'){
+  return 'Thank you.'
+
+}else {
+return 'Bye.'
+}
+
 }
